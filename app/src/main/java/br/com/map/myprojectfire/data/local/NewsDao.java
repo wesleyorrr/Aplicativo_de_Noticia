eef04d565829c452bc791810be6aa8b888d260ff
wesle_p1v78x7 <wesleyorrr@gmail.com>
2022-06-17 10:17:03 -0300

    funcionalidade de banco newdao

---
+++ app/src/main/java/br/com/map/myprojectfire/data/local/NewsDao.java
@@ -0,0 +1,20 @@
+package br.com.map.myprojectfire.data.local;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+import br.com.map.myprojectfire.domain.News;
+
+@Dao
+public interface NewsDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+void save(News news);
+
+    @Query("SELECT * FROM news WHERE favorite = 1")
+    List<News> loandFavoriteNews();
+
+}
